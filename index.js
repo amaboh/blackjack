2f43e13da46c192a36602865c29b0292f85569a0
amaboh <amabocanon@gmail.com>
2022-01-18 22:03:28 +0100

    updates index.js

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-let firstCard =  2
-let secondCard = 11
-let cards = [ firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0 
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = " "
 let messageEl = document.getElementById("message-mi")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
 
+function getRandomCard (){
+
+    let randomNum= Math.floor(Math.random() * 13) + 1
+    if (randomNum > 10){
+        return 10
+    } else if (randomNum === 1){
+        return 11
+    } else {
+        return randomNum
+    } 
+}
+
+
 function startG(){
+    let isAlive = true
+    let  firstCard = getRandomCard()
+    let  secondCard = getRandomCard()
+    cards = [ firstCard, secondCard ]
+    let sum = firstCard + secondCard
     renderGame()
 }
+
 
 function renderGame() {
     sumEl.textContent = "Sum: " + sum
@@ -31,8 +48,9 @@ messageEl.textContent = message
 }
 
 function newCard() {
-    let nuCard = 8
+    let nuCard = getRandomCard()
     sum += nuCard
     cards.push(nuCard)
     renderGame()
+    nuCard = 0
 }
